e0006f1b094dd308e0b2af1029dc89a19ccccb58
Tomas Drozdik <drozdik.tomas@gmail.com>
2018-06-09 17:47:24 +0200

    before branch

--- CYK.hs
+++ CYK.hs
@@ -5,8 +5,7 @@ import Control.Applicative
 type CYKMatrix a = Array (Int, Int) a
 -- ^Primary data structure to perform the CYK algorithm
 
-
 loopArray g a = m
     where m = array bnds $ map f $ assocs a
-        bnds = bounds a
+          bnds = bounds a
 
